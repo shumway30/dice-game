eaa9985572a5ca5977d2a0c79cbb961c85cce60c
Mary Shumway <59614296+shumway30@users.noreply.github.com>
2020-02-18 15:17:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var diceArray = ["", "dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-function rollDice() {
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-  var dice1 = document.querySelector(".img1").setAttribute("src", "images/" + diceArray[randomNumber1]);
-  var dice2 = document.querySelector(".img2").setAttribute("src", "images/" + diceArray[randomNumber2]);
 
-  if (randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerHTML = "Draw!";
-  } else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins!";
-  } else {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏆";
-  }
-  return dice1;
-  return dice2;
-
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🏆 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🏆";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
